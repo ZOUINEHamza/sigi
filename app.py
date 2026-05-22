54063aa47a1860029a3e59a0d794e21750b17f9a
ZOUINEHamza <115801635+ZOUINEHamza@users.noreply.github.com>
2026-05-22 14:20:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -360,8 +360,8 @@ st.markdown(f"""
     <div class="header-box">
         <div style="display: flex; justify-content: space-between; align-items: center;">
             <img src="data:image/png;base64,{st.session_state.get('logo_ern_b64', '')}" width="100">
-            <div style="text-align: center;">
-                <h1 style="margin: 0; font-size: 28px; letter-spacing: 1px; color: {PAL['marine']};">SigNoise </h1>
+            <div style="text-align: center; flex: 1;">
+                <h1 style="margin: 0; font-size: 28px; letter-spacing: 1px; color: {PAL['marine']};">SigNoise</h1>
                 <p style="margin: 5px 0 0 0; color: {PAL['mid']}; font-size: 14px; letter-spacing: 1px;">RF Fingerprinting</p>
             </div>
             <img src="data:image/png;base64,{st.session_state.get('logo_en_b64', '')}" width="100">
