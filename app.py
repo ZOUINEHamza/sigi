d3b930d11bb609d57991b6879a477b59311ba713
ZOUINEHamza <115801635+ZOUINEHamza@users.noreply.github.com>
2026-05-22 14:17:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1031,5 +1031,4 @@ Les hypothèses de travail ont été **validées expérimentalement** :
 
 # --- FOOTER ---
 st.markdown("---")
-st.markdown("<div class='footer'>SigNoise © 2026" \
-"EV2 ZOUINE Hamza </div>", unsafe_allow_html=True)
+st.markdown("<div class='footer' style='text-align:center;'>SigNoise © 2026 </div>", unsafe_allow_html=True)
